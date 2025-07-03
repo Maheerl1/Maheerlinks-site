749a910f4c156a5a798eb8425106e34ef379708f
Maheerl1 <maheerlinks1@gmail.com>
2025-07-03 09:33:58 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+
+import './index.css';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-500">Welcome to MaheerLinks</h1>
+    </div>
+  );
+}
+
+export default App;
